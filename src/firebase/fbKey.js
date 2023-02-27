36ff43b39239a72e46b5389d3c2ceb9b55627f01
Patrick <patrick.vestal@gmail.com>
2023-02-27 13:19:39 -0800

    init

---
+++ src/firebase/fbKey.js
@@ -0,0 +1,6 @@
+apiKey: "AIzaSyCVCxTCqIckONZyS4d3OCt7SkvnX6WrnjM",
+authDomain: "vue-invoice-app-50b49.firebaseapp.com",
+projectId: "vue-invoice-app-50b49",
+storageBucket: "vue-invoice-app-50b49.appspot.com",
+messagingSenderId: "320353640315",
+appId: "1:320353640315:web:f375aeb960e95866a167de"
